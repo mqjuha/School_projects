5658a8138536dea66b9204b72880c943033a22b8
Julia Harttunen <julia.harttunen@tuni.fi>
2020-01-17 13:29:16 +0200

    Lukusarjapeli

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -3,5 +3,19 @@
 
 int main()
 {
+    int maara;
     std::cout << "How many numbers would you like to have? ";
+    std::cin >> maara;
+
+    for ( int luku = 1; luku <= maara; ++luku) {
+        if ( luku % 21 == 0) {
+            std::cout << "zip boing" << std::endl;
+        } else if (luku % 7 == 0) {
+            std::cout << "boing" << std::endl;
+        } else if (luku % 3 == 0) {
+            std::cout << "zip" << std::endl;
+        } else {
+            std::cout << luku << std::endl;
+        }
+    }
 }

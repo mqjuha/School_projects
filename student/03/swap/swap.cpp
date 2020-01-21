6bf882ede1e92ae8b8fecfa412b0cf250137040a
Julia Harttunen <julia.harttunen@tuni.fi>
2020-01-21 15:59:39 +0200

    Tietoalkioiden sisällön vaihtaminen

---
+++ student/03/swap/swap.cpp
@@ -0,0 +1,10 @@
+#include "swap.hh"
+
+// TODO: Implement swap function here
+
+void swap(int& luku1, int& luku2) {
+        int a = luku2;
+        int b = luku1;
+        luku1 = b;
+        luku2 = a;
+}

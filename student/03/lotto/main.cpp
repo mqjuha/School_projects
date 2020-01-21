c19e439b39d0ad7dec7d60c32e57c33c6e6b2377
Julia Harttunen <julia.harttunen@tuni.fi>
2020-01-21 15:30:45 +0200

    Lottopelejä

--- student/03/lotto/main.cpp
+++ student/03/lotto/main.cpp
@@ -4,16 +4,16 @@ using namespace std;
 
 int pallojen_maara;
 int arvottavien_maara;
-unsigned long int todennakosyys;
-int miinus;
 
-int kertoma(int luku) {
-    int tulos = 1;
+
+unsigned long int kertoma(int luku) {
+    unsigned long int tulos = 1;
 
     while ( luku > 0 ) {
         tulos = tulos * luku;
         --luku;
-    } return tulos;
+    }
+    return tulos;
 }
 
 int main()
@@ -26,17 +26,15 @@ int main()
 
     if (pallojen_maara < 0 or arvottavien_maara < 0) {
         cout << "The number of balls must be a positive number." << endl;
-    } else if (pallojen_maara == arvottavien_maara) {
+    } else if (pallojen_maara < arvottavien_maara) {
         cout << "The maximum number of drawn balls is the total amount of balls." << endl;
     } else {
 
         unsigned long int n = kertoma(pallojen_maara);
         unsigned long int p = kertoma(arvottavien_maara);
+        unsigned long int np = kertoma(pallojen_maara - arvottavien_maara);
 
-        miinus = pallojen_maara - arvottavien_maara;
-        unsigned long int erotus = kertoma(miinus);
-
-        todennakosyys = n / (erotus * p);
+        unsigned long int todennakosyys = n / (np * p);
 
         cout << "The probability of guessing all " << arvottavien_maara << " balls correctly is 1/" << todennakosyys << endl;
     }

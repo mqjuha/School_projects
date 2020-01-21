8e986d649a17b9180f9162492f0fc906b5fd7d4d
Julia Harttunen <julia.harttunen@tuni.fi>
2020-01-21 13:33:34 +0200

    Kuutio

--- student/02/cube/main.cpp
+++ student/02/cube/main.cpp
@@ -2,8 +2,17 @@
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
+int luku;
+
+int main(){
+
+    cout << "Enter a number: ";
+    cin >> luku;
+
+    if (luku * luku * luku >= 0) {
+        cout << "The cube of " << luku << " is " << luku * luku * luku << "." << endl;
+
+    } else {
+        cout << "Error! The cube of " << luku << " is not " << luku * luku * luku << "." << endl;
+    }
 }

854b5083f60df5c500a718bce5e577716c05c04a
Julia Harttunen <julia.harttunen@tuni.fi>
2020-01-28 15:25:38 +0200

    Satunnaislukuja

--- student/04/random_numbers/main.cpp
+++ student/04/random_numbers/main.cpp
@@ -7,6 +7,28 @@ using namespace std;
 void produce_random_numbers(unsigned int lower, unsigned int upper)
 {
     // Implement your function here
+    std::default_random_engine rand_gen;
+
+    std::cout << "Enter a seed value or an empty line: ";
+    std::string seed_value = "";
+    getline(std::cin, seed_value);
+    std::cout << endl;
+
+    if( seed_value == "" ) {
+        // If the user did not give a seed value, use computer time as the seed value.
+        rand_gen.seed( time(NULL) );
+    } else {
+        // If the user gave a seed value, use it.
+        rand_gen.seed( stoi(seed_value) );
+    }
+    std::string quit = "";
+    while ( quit == "") {
+        std::uniform_int_distribution<int> distribution(lower, upper);
+        std::cout << "Your drawn random number is " << distribution(rand_gen) << endl;
+        std::cout << "Press enter to continue or q to quit: ";
+        getline(std::cin, quit);
+        std::cout << endl;
+    }
 }
 
 int main()

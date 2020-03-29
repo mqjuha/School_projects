bb09803e0f2dbf8d75f1d49dca4a21df4e59ef00
Julia Harttunen <julia.harttunen@tuni.fi>
2020-03-29 15:15:00 +0300

    joko

--- student/09/cards/cards.cpp
+++ student/09/cards/cards.cpp
@@ -1,13 +1,23 @@
 #include "cards.hh"
 
+// TODO: Implement the methods here
 using namespace std;
 
 Cards::Cards(): top_(nullptr){
 }
 
-void Cards::add(int id){
+Cards::~Cards(){
+    Card_data* tmp = top_;
+    while ( tmp != nullptr ) {
+        top_ = top_->next;
+        delete tmp;
+        tmp = top_;
+    }
+}
 
-    if ( top_ == nullptr ) {
+// Adds a new card with the given id as the topmost element.
+void Cards::add(int id){
+    if(top_ == nullptr){
         top_ = new Card_data{id, nullptr};
         return;
     }
@@ -15,50 +25,50 @@ void Cards::add(int id){
     top_ = tmp;
 }
 
-void Cards::print_from_top_to_bottom(ostream& s){
-
-    Card_data* tmp = top_;
-    int counter = 1;
-
-    while ( tmp != nullptr ) {
-        s << counter << ": " << tmp->data << endl;
-        ++counter;
-        tmp = tmp->next;
-    }
-
+// Prints the content of the data structure with ordinal numbers to the
+// output stream given as a parameter starting from the last element.
+void Cards::print_from_bottom_to_top(std::ostream& s){
+    recursive_print(top_, s);
 }
 
-bool Cards::remove(int& id){
+// Prints the content of the data structure with ordinal numbers to the
+// output stream given as a parameter starting from the first element.
+void Cards::print_from_top_to_bottom(std::ostream& s){
+    Card_data* item_to_be_printed = top_;
+    int running_number = 1;
+    while ( item_to_be_printed != nullptr ) {
+        s << running_number << ": " << item_to_be_printed->data << endl;
+        ++running_number;
+       item_to_be_printed = item_to_be_printed->next;
+    }
+}
 
-    if (top_ == nullptr){
+// Removes the topmost card and passes it in the reference parameter id.
+// Returns false, if the data structure is empty, otherwise returns true.
+bool Cards::remove(int& id){
+    if(top_ == nullptr){
         return false;
     }
     id = top_->data;
     Card_data* new_top = top_->next;
-
     delete top_;
     top_ = new_top;
     return true;
 }
 
+// Moves the last element of the data structure as the first one.
+// Returns false, if the data structure is empty, otherwise returns true.
 bool Cards::bottom_to_top(){
-
-    if (top_ == nullptr){
-        return false;
-    }
-    if(top_->next == nullptr){
+    if(top_ == nullptr || top_->next == nullptr){
         return false;
     }
     Card_data* last = top_;
-
-    while (last->next != nullptr){
-        last = last->next;
+    while (last->next != nullptr) {
+       last = last->next;
     }
     Card_data* new_last = top_;
-
-    while(new_last->next != nullptr){
+    while(new_last->next->next != nullptr){
         new_last = new_last->next;
-
     }
     new_last->next = nullptr;
     last->next = top_;
@@ -66,54 +76,33 @@ bool Cards::bottom_to_top(){
     return true;
 }
 
+// Moves the first element of the data structure as the last one.
+// Returns false, if the data structure is empty, otherwise returns true.
 bool Cards::top_to_bottom(){
-
-    if(top_  == nullptr){
-        return false;
-    }
-    if(top_->next == nullptr){
+    if(top_ == nullptr || top_->next == nullptr){
         return false;
     }
     Card_data* new_top = top_->next;
-
     Card_data* last = top_;
     while(last->next->next != nullptr){
         last = last->next;
     }
-
     top_->next = nullptr;
-    last->next = top_;
-
+    last->next->next = top_;
     top_ = new_top;
     return true;
-
 }
 
-void Cards::print_from_bottom_to_top(ostream &s)
-{
-   recursive_print(top_, s);
-}
-
-Cards::~Cards()
-{
-    Card_data* tmp = top_;
-    while(tmp != nullptr){
-        top_ = top_->next;
-        delete tmp;
-        tmp = top_;
-    }
-}
-
-int Cards::recursive_print(Cards::Card_data *top, ostream &s)
-{
+// You can use the function below in the implementation of
+// the method print_from_bottom_to_top.
+int Cards::recursive_print(Card_data* top, std::ostream& s){
     if(top == nullptr){
         return 1;
-
     }
     int running_number = recursive_print(top->next, s);
     s << running_number << ": " << top->data << endl;
-
-    return running_number + 1;
+     return running_number +1;
 }
+
 
 
